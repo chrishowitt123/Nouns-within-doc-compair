55397f11795e615df3d925f7811b62fab2ede417
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2020-09-17 13:15:32 +0100

    Update Similar Nouns Compair.py

--- Similar Nouns Compair.py
+++ Similar Nouns Compair.py
@@ -1,16 +1,16 @@
+
 import docx2txt
-import itertools
+from collections import Counter
 import pandas as pd
-import re
+from textblob import TextBlob
+import itertools
 from rapidfuzz import fuzz
-from nltk.tokenize import PunktSentenceTokenizer
+from rapidfuzz import process
 from termcolor import colored
-import tkinter as tk
 from tkinter import filedialog
 
-
 print( "\n")  
-print("Welcome to SentSim!")
+print("Welcome to NounSim!")
 print( "\n")  
 print("""     ______ ______
     _/      Y      \_
@@ -40,48 +40,74 @@ print( "\n")
 print( "\n")  
 print( "\n")  
 print( "\n")   
+    
+text =  docx2txt.process(file)
+blob = TextBlob(text)    
+nouns = blob.noun_phrases
+orderedNouns = Counter(nouns).most_common()
 
-text = docx2txt.process(file)
-sent_tokenizer = PunktSentenceTokenizer(text)
-sents = sent_tokenizer.tokenize(text)
-sents = set(sents)
+nouns = []
+values= []
+
+
+for n, v in orderedNouns:
+    nouns.append(n)
+    values.append(v)
+
+orderedNouns = pd.DataFrame(list(zip(nouns, values)),
+              columns=['Nouns','Score'])
 
 x_list = []
 y_list = []
 score = []
 
-for x,y in itertools.combinations(sents, 2):
+
+for x,y in itertools.combinations(nouns, 2):
     fuzz.ratio(x, y)
     score.append(fuzz.ratio(x, y))
     x_list.append(x)
     y_list.append(y)
     
-# remove consecutive blank lines
-
-x_list1 = []  
-for x in x_list:
-
-    xn = re.sub(r'\n\s*\n', '\n\n', x)
-    x_list1.append(xn)
-    
-y_list1 = []  
-for y in y_list:
-
-    yn = re.sub(r'\n\s*\n', '\n\n', y)
-    y_list1.append(yn)
-    
-data_tuples = list(zip(x_list1,y_list1,score))
+data_tuples = list(zip(x_list,y_list,score))
 
 results = pd.DataFrame(data_tuples, columns=['X','Y', 'Score'])  
-
 results = results.sort_values(by=['Score'], ascending=False)
-results = results[results['Score'] > 60]
+results = results[results['Score'] > 70]
+results.to_csv('nounsSim.csv')
+
+
+# In[23]:
+
+
+orderedNouns
+
+
+# In[26]:
+
+
+import itertools
+from rapidfuzz import fuzz
+from rapidfuzz import process
+
+x_list = []
+y_list = []
+score = []
+
+
+for x,y in itertools.combinations(nouns, 2):
+    fuzz.ratio(x, y)
+    score.append(fuzz.ratio(x, y))
+    x_list.append(x)
+    y_list.append(y)
+    
+data_tuples = list(zip(x_list,y_list,score))
+
+results = pd.DataFrame(data_tuples, columns=['X','Y', 'Score'])  
+results = results.sort_values(by=['Score'], ascending=False)
+results = results[results['Score'] > 70]
 
 x_list3 = list(results['X'])
 y_list3 = list(results['Y'])
-        
-    
-# uncommon words
 
 diffs = []
 
@@ -148,3 +174,6 @@ while n <= len(resultsXlist) - 1:
     print( "\n")
     
     n = n+1
+
+
+# In[ ]:
